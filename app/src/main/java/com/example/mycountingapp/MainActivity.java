4940c3bbec570d07239d8597d58547f8e937258b
Jeanine Rioux <jrioux@mvusd.net>
2020-09-28 13:41:05 -0700

    Can now count up

--- app/src/main/java/com/example/mycountingapp/MainActivity.java
+++ app/src/main/java/com/example/mycountingapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.mycountingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int num1 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickFirstButton(View view)
     {
-        Toast.makeText(this,"You clicked the first button", Toast.LENGTH_LONG).show();
+        num1++;
+        TextView introTextView = (TextView) findViewById(R.id.introTV);// the introTV is the name of the TV to be updated
+        introTextView.setText("You now have " + num1 + " fishies.");
+       // Toast.makeText(this,"You clicked the first button", Toast.LENGTH_LONG).show();
     }
 
     public void onClickSecondButton(View view)
@@ -37,6 +44,21 @@ public class MainActivity extends AppCompatActivity {
     {
         startActivity(new Intent(MainActivity.this, Screen2.class));
     }
+ /*
+    public void openDialogue(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Confirm reset count");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                Toast.makeText(MainActivity.this, "yessiree", Toast.LENGTH_LONG).show();
+
+            }
+        })
+    }
+
+  */
 
 
 }
